0d08db63ad7ab9ca991a59a9cc95ba9a39a3d0a8
huang900107@163.com <feiping.huang@yottaa.com>
2013-09-10 14:57:45 +0800

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 require_dependency 'constraints/format'
 EmberTodo::Application.routes.draw do
+  root :to => redirect('/projects')
   get '*tab', :to => 'application#index', constraints: Constraints::Format.new(:html)
 
   constraints(constraints: Constraints::Format.new(:json)) do
